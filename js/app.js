068b312c11800ffcfadc3b4350e887489588f5c3
francesyu90 <francesyu90@yahoo.com>
2016-07-12 13:16:44 -0700

    Update pagination links depending on the number of search results

--- js/app.js
+++ js/app.js
@@ -1,7 +1,14 @@
-var studentList = document.getElementsByClassName("student-item");
-var numOfPages = Math.ceil(studentList.length/10);
+var studentItems = document.getElementsByClassName("student-item");
+var defaultStudentItems = document.getElementsByClassName("student-item");
+var numOfPages = 0;
 var currentPage = 0;
 
+//  calculate the number of pages needed based on the number of students
+var calculateNumOfPages = function(){
+    return Math.ceil(studentItems.length/10);
+}
+
+//  generate a list of pagination links (li elements only)
 var createLinks = function(){
     var links = [];
     for(var i = 0; i < numOfPages; i++) {
@@ -15,6 +22,7 @@ var createLinks = function(){
     return links;
 }
 
+// append li elements to ul
 var appendLinks = function(links) {
     var ul = document.createElement("ul");
     for(var i = 0; i < numOfPages; i++){
@@ -24,7 +32,9 @@ var appendLinks = function(links) {
     return ul;
 }
 
+// create pagination and add it to the page
 var createPagination = function() {
+    numOfPages = calculateNumOfPages();
     var pagination = document.createElement("div");
     var page = document.querySelector(".page");
     var links = createLinks();
@@ -34,6 +44,7 @@ var createPagination = function() {
     page.appendChild(pagination);
 }
 
+//  show students according to which pagination link is clicked
 var paginationOnClick = function() {
     $(".pageLink").on("click", function(event){
         $(this).siblings().children().each(function(){
@@ -41,12 +52,13 @@ var paginationOnClick = function() {
         });
         $(this).children().first().addClass("active");
         currentPage = parseInt($(this).children().first().text());
-        $(studentList).slice(0, (currentPage-1)*10).hide();
-        $(studentList).slice(currentPage*10, numOfPages*10).hide();
-        $(studentList).slice((currentPage-1)*10, currentPage*10).show();
+        $(studentItems).slice(0, (currentPage-1)*10).hide();
+        $(studentItems).slice(currentPage*10, numOfPages*10).hide();
+        $(studentItems).slice((currentPage-1)*10, currentPage*10).show();
     });
 }
 
+// create search bar and add it to the page
 var createSearchBar = function() {
     var pageHeader = $(".page-header");
     var searchBar = document.createElement("div");
@@ -61,11 +73,53 @@ var createSearchBar = function() {
     pageHeader.append(searchBar);
 }
 
-$(document).ready(function(){
+
+var searchBarOnClick = function() {
+    $(".student-search button").on("click", function(){
+        var targetText = $(this).prev().val().toLowerCase().trim();
+        $(this).prev().val("");
+        if(targetText === "") {
+            currentPage = 0;
+            studentItems = defaultStudentItems;
+            resetstudentItems();
+        } else {
+            var matchedCount = 0;
+            var list = [];
+            $(studentItems).each(function(){
+                if($(this).is(":contains('" + targetText + "')")){
+                    matchedCount++;
+                    list.push(this);
+                    $(this).show();
+                }else{
+                    $(this).hide();
+                }
+            });
+            $(".pagination").remove();
+            if(matchedCount == 0){
+                alert("No results found!");
+            } else if (matchedCount >= 10){
+                studentItems = list;
+                currentPage = 0;
+                resetstudentItems();
+            }
+        }
+    })  
+}
+
+var resetstudentItems = function(){
     createPagination();
-    createSearchBar();
     paginationOnClick();
     $(".pageLink")[currentPage].click();
+}
+
+var includeSearchBar = function(){
+    createSearchBar();
+    searchBarOnClick();
+}
+
+$(document).ready(function(){
+    resetstudentItems();
+    includeSearchBar();
 });
 
 
